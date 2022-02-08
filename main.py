57222aad6f579c5056adf19eff498d09cc7347fc
Yisel <98533760+YiselFlores@users.noreply.github.com>
2022-02-08 17:25:26 +0000

    code

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-mylist = []
+mylist = [0,0,0,0]
 userinput = input("enter a number ")
 mylist[0]= userinput
-print(mylist)
 userinput1 = input("enter a second number ")
 mylist[1]= userinput1
 userinput2 = input("enter third number ")
 mylist[2]= userinput2
 userinput3 = input("enter a fourth number ")
 mylist[3]= userinput3
+print(mylist)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+mylist[0]=mylist[3]
+mylist[3]=mylist[0]
+print(mylist)
+
+#userinput = input("enter a number ")
+#userinput1 = input("enter a second number ")
+#userinput2 = input("enter third number ")
+#userinput3 = input("enter a fourth number ")
+#mylist = [userinput, userinput1,userinput2, userinput3]
+#print(mylist)
+#print(mylist[0])
+#print(mylist[1])
+#print(mylist[2])
+#print(mylist[3])
+
